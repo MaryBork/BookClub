4bdbf979a916a87989533e5a4879bdeeadaac3db
Meghan Stang <mstang15@gmail.com>
2018-10-09 19:49:27 -0600

    adds validation and relationship testing

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-describe Artist, type: :model do
+describe Author, type: :model do
   describe 'Validations' do
     it {should validate_presence_of(:name)}
   end
   describe 'Relationships' do
-    it {should have_many(:books)}
+    it {should have_many(:book_authors)}
+    it {should have_many(:books).through(:book_authors)}
   end
 end
