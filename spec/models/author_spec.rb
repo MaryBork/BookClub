1b107e2ef22bfbcd939781dcc6497129d1b6c1f4
Meghan Stang <mstang15@gmail.com>
2018-10-09 19:33:51 -0600

    adds author model spec

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Artist, type: :model do
+  describe 'Validations' do
+    it {should validate_presence_of(:name)}
+  end
+  describe 'Relationships' do
+    it {should have_many(:books)}
+  end
+end
