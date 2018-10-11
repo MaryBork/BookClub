5c52b467e5e405148a10575f25256e41c97480e3
Meghan Stang <mstang15@gmail.com>
2018-10-11 14:41:13 -0600

    adds book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ has_many :authors, through: :book_authors
 has_many :users, through: :reviews
 
 def self.sorted_by(direction,sort)
-  sorted_books = select('books.*, avg(score) AS average_score').joins(:reviews).group(:id, :book_id)
+  sorted_books = select('books.*, avg(score) AS average_score').joins(:reviews).group(:book_id,:id)
   if direction && sort
     sorted_books = sorted_books.order("#{sort} #{direction}")
   end
@@ -14,11 +14,11 @@ def self.sorted_by(direction,sort)
 end
 
 def highest_rated
-  reviews.order(score: :DESC).take(3)
+  reviews.order(score: :DESC).limit(3) 
 end
 
 def lowest_rated
-  reviews.order(score: :ASC).take(3)
+  reviews.order(score: :ASC).limit(3)
 end
 
 end
