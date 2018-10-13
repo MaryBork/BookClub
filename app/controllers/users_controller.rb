4f73020c3bb609225594f49b3ba5e324d5742bba
Meghan Stang <mstang15@gmail.com>
2018-10-13 09:46:59 -0600

    ready to merge with new book page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @show_user = User.find(params[:id])
-    @user_chronological_reviews = User.chronological_order_reviews)
+    @user_chronological_reviews = User.chronological_order_reviews
   end
 
 end
